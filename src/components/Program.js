74d3e6a262c24c0b56a8b967f8e55d31a20852c7
Sébastien Mantanus <sebastienmantanus@iMac-de-Sebastien.local>
2022-12-19 08:57:04 +0100

    ternaires improvement

--- src/components/Program.js
+++ src/components/Program.js
@@ -11,13 +11,8 @@ const Program = (props) => {
 
         <div className="bottomDetails">
           <span>{props.duration}</span>
-          <span>
-            {props.direct ? <p className="red"> • Direct</p> : <p></p>}
-          </span>
-          <span>
-            {props.isUnseen ? <p className="red"> • Inédit</p> : <p></p>}
-          </span>
-          {/* <span className="red">"Inédit"</span> */}
+          <span>{props.direct && <p className="red"> • Direct</p>}</span>
+          <span>{props.isUnseen && <p className="red"> • Inédit</p>}</span>
         </div>
       </div>
     </div>
